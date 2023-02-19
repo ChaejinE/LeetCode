b53ac431f9164d121933516f0cc52588a1d94b3d
JeongChaeJin <69780812+ChaejinE@users.noreply.github.com>
2023-02-19 09:32:41 +0000

    75. sort colors

---
+++ 01-array/05-sort-colors/05-sort-colors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int writeIdx = 0;
+        int zeroIdx = 0;
+        int twoIdx = nums.size() - 1;
+
+        while(writeIdx <= twoIdx)
+        {
+            if (nums[writeIdx] == 0)
+            {
+                swap(nums[writeIdx++], nums[zeroIdx++]);
+            }
+            else if (nums[writeIdx] == 2)
+            {
+                swap(nums[writeIdx], nums[twoIdx--]);
+            }
+            else
+            {
+                ++writeIdx;
+            }
+        }
+    }
+};
