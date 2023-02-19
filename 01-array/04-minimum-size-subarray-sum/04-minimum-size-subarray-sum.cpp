9308925d36b4b7d2287a4fdb12ec132dd8c53997
JeongChaeJin <69780812+ChaejinE@users.noreply.github.com>
2023-02-19 09:35:43 +0000

    209. minimum size subarray sum

--- 01-array/04-minimum-size-subarray-sum/04-minimum-size-subarray-sum.cpp
+++ 01-array/04-minimum-size-subarray-sum/04-minimum-size-subarray-sum.cpp
@@ -1,24 +1,22 @@
 class Solution {
-    public:
-    int minSubArrayLen(int target, vector < int > & nums) {
-        int startIdx = 0
-        int sum = 0
-        int minLength = numeric_limits < int > : : max()
-
-        for (int endIdx=0
-             endIdx < nums.size()
-             + +endIdx)
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int startIdx = 0;
+        int sum = 0;
+        int minLength = numeric_limits<int>::max();
+        
+        for (int endIdx = 0; endIdx < nums.size(); ++endIdx)
         {
-            sum += nums[endIdx]
+            sum += nums[endIdx];
 
             while (sum >= target)
             {
-                minLength = min(minLength, endIdx - startIdx + 1)
-                sum -= nums[startIdx++]
+                minLength = min(minLength, endIdx - startIdx + 1);
+                sum -= nums[startIdx++];
             }
         }
 
-
-        return (minLength != numeric_limits < int >: : max()) ? minLength : 0
+        
+        return (minLength != numeric_limits<int>::max()) ? minLength : 0;
     }
 };
