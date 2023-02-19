8ba64d48fd4643529c6d52ac38e45ba48ef51fb0
JeongChaeJin <69780812+ChaejinE@users.noreply.github.com>
2023-02-19 08:46:03 +0000

    209 minimum size subarray length

---
+++ 01-array/04-minimum-size-subarray-sum/04-minimum-size-subarray-sum.py
@@ -0,0 +1,24 @@
+class Solution {
+    public:
+    int minSubArrayLen(int target, vector < int > & nums) {
+        int startIdx = 0
+        int sum = 0
+        int minLength = numeric_limits < int > : : max()
+
+        for (int endIdx=0
+             endIdx < nums.size()
+             + +endIdx)
+        {
+            sum += nums[endIdx]
+
+            while (sum >= target)
+            {
+                minLength = min(minLength, endIdx - startIdx + 1)
+                sum -= nums[startIdx++]
+            }
+        }
+
+
+        return (minLength != numeric_limits < int >: : max()) ? minLength : 0
+    }
+};
