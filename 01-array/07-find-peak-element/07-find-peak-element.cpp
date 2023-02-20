0bedc3680d792d0214fbc9f050c7e9e2f714d171
JeongChaeJin <69780812+ChaejinE@users.noreply.github.com>
2023-02-20 14:31:53 +0000

    162. find peak element

---
+++ 01-array/07-find-peak-element/07-find-peak-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if (nums.size() == 1)
+            return 0;
+
+        int leftIdx = 0;
+        int rightIdx = nums.size() -1;
+        int pivotIdx;
+        while (leftIdx < rightIdx)
+        {
+            pivotIdx = (leftIdx + rightIdx) / 2;
+
+            if (nums[pivotIdx] <= nums[pivotIdx + 1])
+                leftIdx = pivotIdx + 1;
+            else
+                rightIdx = pivotIdx;
+        }
+
+        return leftIdx;
+    }
+};
