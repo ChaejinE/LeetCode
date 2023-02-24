5cb5f5d55dc31cbd2e5a0adf1dfdd4606f936051
JeongChaeJin <69780812+ChaejinE@users.noreply.github.com>
2023-02-24 14:32:48 +0000

    287. find the duplicate number

---
+++ 01-array/10-find-the-duplicate-number/10-find-the-duplicate-numer.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        int result;
+        for (auto e : nums)
+        {
+            if (nums[abs(e)] < 0)
+            {
+                result = e; // - 면 이미 marking 된 중복..
+                break;
+            }
+            else
+            {
+                nums[abs(e)] *= -1; // 해당 int 값을 index 정보로 보고, 존재했음을 marking
+            }
+        }
+
+        return result < 0 ? result * -1 : result;
+    }
+};
