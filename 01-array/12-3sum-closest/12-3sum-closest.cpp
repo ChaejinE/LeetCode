5194e82e7863c84370ef9de8babcf1928879738b
JeongChaeJin <69780812+ChaejinE@users.noreply.github.com>
2023-02-25 07:09:44 +0000

    16. 3Sum closetst

---
+++ 01-array/12-3sum-closest/12-3sum-closest.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+public:
+    int threeSumClosest(vector<int> &nums, int target)
+    {
+        int result = numeric_limits<int>::max() - 1e4;
+        sort(nums.begin(), nums.end());
+
+        for (int firstIdx = 0; firstIdx < nums.size() - 2; ++firstIdx)
+        {
+            if (firstIdx > 0 && nums[firstIdx] == nums[firstIdx - 1])
+                continue;
+
+            int secondIdx = firstIdx + 1;
+            int thirdIdx = nums.size() - 1;
+
+            for (; secondIdx < thirdIdx;)
+            {
+                int sum = nums[firstIdx] + nums[secondIdx] + nums[thirdIdx];
+
+                if (sum < target)
+                {
+                    ++secondIdx;
+                }
+                else if (sum > target)
+                {
+                    --thirdIdx;
+                }
+                else
+                {
+                    return sum;
+                }
+
+                if (abs(target - sum) < abs(target - result))
+                    result = sum;
+            }
+        }
+
+        return result;
+    }
+};
